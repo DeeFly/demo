9e71dc9ead09bd8a62c27e4523702a38646227e9
高庆明 <gaoqingming@xueleyun.com>
2019-01-24 10:41:23 +0800

    completableFutureTest

--- info-demo/src/main/java/com/gaofei/App.java
+++ info-demo/src/main/java/com/gaofei/App.java
@@ -1,25 +1,12 @@
 package com.gaofei;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
-
 /**
  * Hello world!
  */
 public class App {
 
     public static void main(String[] args) {
-        List<String> stringList = new ArrayList<String>();
-
-        stringList.add("abc");
-        stringList.add("def");
-        stringList.forEach((value) -> {});
-
-        Stream<String> stream = stringList.stream();
-
-        Stream<String> streamPeeked = stream.peek((value) -> {
-            System.out.println("value");
-        });
+        Object o = null;
+        System.out.println(o + "haha");
     }
 }
---
+++ info-demo/src/main/java/com/gaofei/Thread/CompletableFutureTest.java
@@ -0,0 +1,57 @@
+package com.gaofei.Thread;
+
+
+import org.springframework.util.StopWatch;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
+/**
+ * 结果是有序的，按提交的顺序返回结果
+ * Created by GaoQingming on 2019/1/24 0024.
+ */
+public class CompletableFutureTest {
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        List<Shop> shops = new ArrayList<>(10);
+        shops.add(new Shop("1"));
+        shops.add(new Shop("2"));
+        shops.add(new Shop("3"));
+        shops.add(new Shop("4"));
+        shops.add(new Shop("5"));
+        shops.add(new Shop("6"));
+        shops.add(new Shop("7"));
+        shops.add(new Shop("8"));
+        shops.add(new Shop("9"));
+        shops.add(new Shop("10"));
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        List<CompletableFuture> completableFutures = shops.stream().map(shop -> CompletableFuture.supplyAsync(() -> shop.getPrice(), executorService)).collect(Collectors.toList());
+        List<String> result = completableFutures.stream().map(CompletableFuture<String>::join).collect(Collectors.toList());
+        stopWatch.stop();
+        System.out.println(stopWatch.getTotalTimeSeconds());
+        result.forEach(System.out::println);
+    }
+
+    static class Shop{
+        private String name;
+        public Shop(String name) {
+            this.name = name;
+        }
+
+        public String getPrice() {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return new Random().nextInt() + "  " + this.name;
+        }
+    }
+}
