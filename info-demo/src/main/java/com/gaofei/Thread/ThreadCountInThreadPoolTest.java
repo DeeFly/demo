144f481dac31f45ab89989eaf13e7e20567630b2
高庆明 <gaoqingming@xueleyun.com>
2019-03-07 15:01:07 +0800

    extends ThreadPool

--- info-demo/src/main/java/com/gaofei/Thread/ThreadCountInThreadPoolTest.java
+++ info-demo/src/main/java/com/gaofei/Thread/ThreadCountInThreadPoolTest.java
@@ -1,12 +1,14 @@
 package com.gaofei.Thread;
 
-import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.*;
 
 /**
  * 使用 new SynchronousQueue的时候，来任务的时候，只要线程数未到达maximumPoolSize，就会创建新的线程
  * SynchronousQueue不存储任务，而是直接交付给线程池中的线程执行，所以如果线上数达到线程池上限且都在工作，新进的任务会执行丢弃策略。
  * 而如果使用LinkedBlockingQueue的时候，只有队列满了以后，才会创建大于corePoolSize的线程数。
+ *
+ * 注意：工厂一定要接受r（Runnable），任务，不然会把任务丢弃。
  * Created by GaoQingming on 2019/3/6 0006.
  */
 public class ThreadCountInThreadPoolTest {
@@ -14,8 +16,8 @@ public class ThreadCountInThreadPoolTest {
 
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newCachedThreadPool();
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(4, 10, 20, TimeUnit.SECONDS, new SynchronousQueue<>(),
-                (r) -> new Thread("MyThread" + String.valueOf(++threadCount)),
+        ThreadPoolExecutor threadPoolExecutor = new MyThreadPoolExecutor(4, 10, 20, TimeUnit.SECONDS, new SynchronousQueue<>(),
+                (r) -> new Thread(r, "MyThread" + String.valueOf(++threadCount)),
                 (r, executor) -> {
                     System.out.println("over flow !");
                 });
@@ -35,12 +37,25 @@ public class ThreadCountInThreadPoolTest {
             threadPoolExecutor.execute(runnable);
         }
 
-
         System.out.println(threadPoolExecutor.getActiveCount());
-        try {
-            System.in.read();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+        System.exit(0);
     }
 }
+
+ class MyThreadPoolExecutor extends ThreadPoolExecutor{
+
+     public MyThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory, RejectedExecutionHandler handler) {
+         super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
+     }
+
+     public void shutdown() {
+         System.out.println("shutdown called.");
+         super.shutdown();
+     }
+
+     public List<Runnable> shutdownNow() {
+         System.out.println("shutdownNow called.");
+         return super.shutdownNow();
+     }
+ }
